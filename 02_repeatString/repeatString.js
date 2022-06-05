252e4607a7371fa05851f534f6e827e146fde03f
KevinRius <k.riou29@gmail.com>
2022-06-05 19:08:38 +0200

    resolve problem with an if and a loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let s = string;
+    let ss = '';
+    if (num < 0) {
+        ss = 'ERROR';
+    } else {
+        for (i = 0; i < num; i++) {
+            ss += s;
+        }
+    }
+    return ss;
 };
 
 // Do not edit below this line
