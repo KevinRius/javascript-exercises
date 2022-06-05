5abc82507c5959784cf7d75a10d227f792a0b6e0
KevinRius <k.riou29@gmail.com>
2022-06-05 21:55:52 +0200

    sovle problem through a double loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+    for (i = 0; i < num.length; i++) {
 
+        for (o = 0; o < arr.length; o++) {
+            if (arr[o] === num[i]) {
+                arr.splice(o, 1);
+            }
+        }
+
+    }
+    return arr;
 };
 
 // Do not edit below this line
