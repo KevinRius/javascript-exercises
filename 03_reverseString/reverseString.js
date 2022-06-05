8b38b1f128746b04c35cbea159e79e3cc3aeac02
KevinRius <k.riou29@gmail.com>
2022-06-05 19:36:57 +0200

    resolve problem, array > reverse > reform string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let string = str;
+    let revString = "";
+    const arr = string.split("");
+    arr.reverse();
+    
+    for (i = 0; i < arr.length; i++) {
+        revString += arr[i];
+    }
+    return revString;
 };
 
 // Do not edit below this line
