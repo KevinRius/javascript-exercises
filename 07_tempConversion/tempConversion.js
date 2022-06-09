5ae43e22ec41854f738c34afd21415984cbef5a5
KevinRius <k.riou29@gmail.com>
2022-06-09 17:53:29 +0200

    sovled by applying formula

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(t) {
+  return Math.round(((t - 32) * 0.5556) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(t) {
+  return Math.round(((t * 1.8) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
