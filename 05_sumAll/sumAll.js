ef355291f6c35ee19a1f96be9e3a698f5ec1bc70
KevinRius <k.riou29@gmail.com>
2022-06-09 17:11:15 +0200

    solve problem through condition and loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(f, s) {
+    let r = 0;
+    let low = 0;
+    let high = 0;
 
+    if (typeof(s) !== "number") {
+        return "ERROR";
+    } else if (f < 0 || s < 0) {
+        return "ERROR";
+    } else if (f < s) {
+        low = f;
+        high = s;
+    } else {
+        low = s;
+        high = f;
+    }
+
+    for (i = low; i <= high; i++) {
+        r += i;
+    }
+    return r;
 };
 
 // Do not edit below this line
